41ca45c25379244086893e234ebf10793d10a00b
CHELSEA <CEFERNAN@UVM.EDU>
2024-10-02 10:32:30 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//initial commit//
+//sea otter//
+//properties//
+    //name is Brutus, fluffy and shiny, swims around, has a favorite rock//
+
+//events// 
+    //sleeps//
+
+//methods//
+    //floats on back and hold hands with its favorite sea otter//
